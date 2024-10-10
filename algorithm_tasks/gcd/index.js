51fd96ca2704830ef240acdffb6ae7632626d84e
Viktor <palchinskij19@gmail.com>
2024-10-10 15:19:57 +0300

    10.10.24 - gcb

---
+++ algorithm_tasks/gcd/index.js
@@ -0,0 +1,15 @@
+const gcd = (a, b) => {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('Both arguments must be numbers.');
+    }
+
+    if (b === 0) return Math.abs(a);
+
+    return gcd(b, a % b);
+};
+
+console.log(gcd(48, 18)); 
+console.log(gcd(56, 98)); 
+console.log(gcd(101, 10)); 
+console.log(gcd(0, 5)); 
+console.log(gcd(-36, 60));
